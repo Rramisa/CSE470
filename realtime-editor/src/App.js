1e6b47590579986cebf3f011d240c91c09b873e9
Rramisa <ramisa.rahman1@g.bracu.ac.bd>
2025-08-04 02:37:38 +0600

    Worked on editing design

--- realtime-editor/src/App.js
+++ realtime-editor/src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/home'
 import Editorpage from './pages/editorpage'
+import { Toaster } from 'react-hot-toast';
+
+
 function App() {
   return (
      <>
+      <div>
+        <Toaster position="top-center" toastOptions={{
+          sucess:{
+            theme:{
+              primary: '#4aed88'
+            }
+          }
+        }}></Toaster>
+      </div>
        <BrowserRouter>
          <Routes> 
            <Route path="/" element={<Home/>}></Route>
---
+++ realtime-editor/src/components/editor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const editor = () => {
+  return (
+    <div>
+      editor goes here
+    </div>
+  )
+}
+
+export default editor
